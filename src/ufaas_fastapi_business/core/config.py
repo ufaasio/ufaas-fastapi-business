16f2e89f2e05dae8452509dba2fc9d0c4091dde5
Mahdi Kiani <mahdikiany@gmail.com>
2024-12-21 13:45:10 +0000

    .

--- src/ufaas_fastapi_business/core/config.py
+++ src/ufaas_fastapi_business/core/config.py
@@ -5,7 +5,11 @@ from fastapi_mongo_base.core.config import Settings as BaseSettings
 
 class Settings(BaseSettings):
     business_domains_url: str = (
-        "https://business.ufaas.io/api/v1/apps/business/businesses/"
+        os.getenv(
+            "UFAAS_BUSINESS_DOMAINS_URL",
+            "https://business.ufaas.io/api/v1/apps/business",
+        )
+        + "/businesses/"
     )
     usso_app_auth_url: str = os.getenv(
         "USSO_APP_AUTH_URL", "https://sso.ufaas.io/app-auth/access"
