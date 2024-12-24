a083592f705d61d15bc2de9870d3df05e79c96cf
Mahdi Kiani <mahdikiany@gmail.com>
2024-12-24 06:07:11 +0000

    add app_secret

--- src/ufaas_fastapi_business/core/config.py
+++ src/ufaas_fastapi_business/core/config.py
@@ -14,3 +14,8 @@ class Settings(BaseSettings):
     usso_app_auth_url: str = os.getenv(
         "USSO_APP_AUTH_URL", "https://sso.ufaas.io/app-auth/access"
     )
+
+    app_id: str = os.getenv("APP_ID")
+    app_secret: str = os.getenv("APP_SECRET")
+    app_scopes: str = os.getenv("APP_SCOPES", default="[]")
+    app_auth_expiry: int = 60  # 1 minute
